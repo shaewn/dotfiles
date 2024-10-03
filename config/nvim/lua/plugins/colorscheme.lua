d18d094e8519c57d324d428985d0fb3b7174c4f6
Sean <seanm6407@gmail.com>
2024-10-03 14:21:34 -0500

    added neovim config.

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "embark"
+        }
+    },
+
+    {
+        "ellisonleao/gruvbox.nvim"
+    },
+    {
+        "embark-theme/vim"
+    }
+}
