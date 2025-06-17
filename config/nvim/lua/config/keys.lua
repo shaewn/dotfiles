c24bb2598a8747081ea8891383c530be10dacd44
Sean McCain <seanm6407@gmail.com>
2025-06-16 22:03:59 -0500

    Fix dir stack stuff

--- config/nvim/lua/config/keys.lua
+++ config/nvim/lua/config/keys.lua
@@ -45,7 +45,13 @@ vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>")
 
 -- Telescope (Basic)
 vim.keymap.set("n", "<leader>.", "<cmd>Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>,", "<cmd>Telescope buffers<CR>")
+vim.keymap.set("n", "<leader>,",
+  function()
+    require("telescope.builtin").buffers({
+      sort_mru = true,
+    })
+  end
+)
 vim.keymap.set("n", "<leader>tb", "<cmd>Telescope builtin<CR>")
 vim.keymap.set("n", "<leader>tg", "<cmd>Telescope git_files<CR>")
 vim.keymap.set("n", "<leader>t/", "<cmd>Telescope live_grep<CR>")
@@ -77,50 +83,74 @@ local function print_cwd()
   vim.print(vim.fn.getcwd())
 end
 
+---@type string[]
 local dir_stack = {}         -- stack of previous directories
-local popped_dirs_stack = {} -- stack of dirs that we popped
+---@type string[]
+local from_dir_stack = {} -- stack of dirs that we popped
 
 ---@param new_dir string
 local function pushdir(new_dir)
   local dir = vim.fn.getcwd()
-  -- NOTE: This function will error if the directory doesn't exist, and our dir_stack will remain unmodified.
-  vim.cmd.cd(new_dir)
-  table.insert(dir_stack, dir)
+
+  if pcall(vim.cmd.cd, new_dir) then
+    table.insert(dir_stack, dir)
+    return true
+  else
+    print("Failed to change to directory", new_dir)
+    return false
+  end
+end
+
+---@param pop_from string[]
+---@param push_to string[]
+local function dirpop(pop_from, push_to)
+  if #pop_from < 1 then
+    print("stack empty!")
+    return false
+  end
+
+  local dir = table.remove(pop_from, #pop_from)
+  local cwd = vim.fn.getcwd()
+  vim.cmd.cd(dir)
+  table.insert(push_to, cwd)
+  return true
 end
 
 local function unpopdir()
-  assert(#popped_dirs_stack >= 1, "stack of popped directories empty!")
-  local dir = table.remove(popped_dirs_stack, #popped_dirs_stack)
-  pushdir(dir)
+  return dirpop(from_dir_stack, dir_stack)
 end
 
 local function popdir()
-  assert(#dir_stack >= 1, "directory stack empty!")
-  local dir = table.remove(dir_stack, #dir_stack)
-  table.insert(popped_dirs_stack, dir)
-  vim.cmd.cd(dir)
+  return dirpop(dir_stack, from_dir_stack)
 end
 
 local function dirfn(func)
   return function()
-    func()
-    print_cwd()
+    local rv = func()
+
+    if rv == nil or rv then
+      print_cwd()
+    end
   end
 end
 
 -- Directories
 vim.keymap.set("n", "<leader>df", dirfn(
-  function() pushdir(vim.fn.expand("%:h")) end
+  function() return pushdir(vim.fn.expand("%:h")) end
 ), { desc = "cd %:h" })
 
 vim.keymap.set("n", "<leader>d-", dirfn(
-  function() popdir() end
-), { desc = "goto previous directory" })
+  function() return popdir() end
+), { desc = "popdir" })
+
+vim.keymap.set("n", "<leader>d+", dirfn(
+  function() return unpopdir() end
+), { desc = "undo popdir" })
 
 vim.keymap.set("n", "<leader>dt", dirfn(
   function()
     local dir = vim.fn.input("Go to directory: ", "", "dir")
-    pushdir(dir)
+    return pushdir(dir)
   end
 ), { desc = "goto named directory" })
 
@@ -129,10 +159,6 @@ vim.keymap.set("n", "<leader>d..", dirfn(
     pushdir("..")
   end
 ), { desc = "goto parent directory" })
-
-vim.keymap.set("n", "<leader>d+", dirfn(
-  function() unpopdir() end
-), { desc = "`undo'" })
 
 vim.keymap.set("n", "<leader>dc", function()
   dir_stack = {}
@@ -148,11 +174,14 @@ vim.keymap.set("n", "<leader>pds", function()
   vim.print(dir_stack)
 end, { desc = "print directory stack" })
 
-vim.keymap.set("n", "<leader>pps", function()
-  vim.print(popped_dirs_stack)
+vim.keymap.set("n", "<leader>pfs", function()
+  vim.print(from_dir_stack)
 end, { desc = "print popped dirs stack" })
 
 -- UI
-vim.keymap.set("n", "<leader>ut", function()
+local function mini_files()
   MiniFiles.open()
-end, { desc = "open file tree (mini.files)" })
+end
+
+vim.keymap.set("n", "<leader>ut", mini_files, { desc = "open file tree (mini.files)" })
+vim.keymap.set("n", "-", mini_files, { desc = "open file tree (mini.files)" })
