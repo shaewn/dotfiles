21565e5870eb4b19185a9915dd5f4512edf9ed60
Sean McCain <seanm6407@gmail.com>
2025-07-21 09:42:36 -0500

    hjkl

--- config/nvim/lua/config/keys.lua
+++ config/nvim/lua/config/keys.lua
@@ -44,10 +44,10 @@ vim.keymap.set("n", "<leader>bo", ":%bd|e#|bd#<CR>", { desc = "buffer equivalent
 vim.keymap.set("n", "<leader>,", telescope_buffers, { desc = "Telescope buffers" })
 
 -- Cursor motion
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
-vim.keymap.set("v", "j", "gj")
-vim.keymap.set("v", "k", "gk")
+-- vim.keymap.set("n", "j", "gj")
+-- vim.keymap.set("n", "k", "gk")
+-- vim.keymap.set("v", "j", "gj")
+-- vim.keymap.set("v", "k", "gk")
 
 -- Visual selection
 vim.keymap.set("v", ">", ">gv")
