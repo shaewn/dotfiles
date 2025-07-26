72720909b095864589af7e408c1fd2fce39aeb31
Sean McCain <seanm6407@gmail.com>
2025-07-26 12:38:44 -0500

    set termguicolors to fix tmux colors

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,11 +1,12 @@
 ---@diagnostic disable: undefined-global
 -- Options
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
-vim.opt.hlsearch = false
-vim.opt.cinoptions = "Ls,l1,g0,N-s,E-s,t0,j1"
+vim.o.tabstop = 8
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 8
+vim.o.hlsearch = false
+vim.o.cinoptions = "Ls,l1,g0,N-s,E-s,t0,j1"
+vim.o.termguicolors = true
